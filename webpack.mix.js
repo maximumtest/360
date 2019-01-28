041d6cf79733901a58427115912741e5ab5c4d58
Igor Baryshev <igor.barishev@maximumtest.ru>
2019-01-28 13:06:28 +0300

    Fixed common boilerplate

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
-
-mix.browserSync(process.env.APP_URL);
-
-mix.disableNotifications();
